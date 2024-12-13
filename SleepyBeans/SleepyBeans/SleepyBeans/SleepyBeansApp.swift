200d026f3ea75f2d73e07cc7f15e5aad89069b1a
Priyankshu Sheet <135867558+priyankshusheet@users.noreply.github.com>
2024-12-13 21:45:38 +0530

    Add files via upload

---
+++ SleepyBeans/SleepyBeans/SleepyBeans/SleepyBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepyBeansApp.swift
+//  SleepyBeans
+//
+//  Created by Priyankshu Sheet on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SleepyBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
